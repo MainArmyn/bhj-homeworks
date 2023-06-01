2956bdd92fb4de8853a0da1fd2e6bbc1e0fb6da0
Савва Саркисян <savvasarkisan@MacBook-Pro-Savva.local>
2023-06-01 14:31:41 +0400

    nine

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,5 +1,6 @@
-function Auth() {
 
+
+function Auth2() {
     function Check() {
         for (let key in localStorage) {
             if (key.startsWith("user" + "-")) {
@@ -8,23 +9,45 @@ function Auth() {
         }
         return null;
     }
+
+    function Incorrect(form) {
+        alert("Неверный логин/пароль");
+                        Array.from(form.children).forEach(element => {
+                            for (let child of element.children) {
+                                child.value = '';
+                            }
+                        });
+    }
+    const xhr = new XMLHttpRequest();
+    const authForm = document.getElementById("signin__form");
+    const welcome  = document.getElementById("welcome");
+    const logOutBtn = document.querySelector(".log-out");
         if (!Check()) {
-            const authForm = document.getElementById("signin__form");
             authForm.addEventListener("submit" ,  function(e) {
                 e.preventDefault();
                 let newForm = new FormData(authForm);
-                fetch("https://students.netoservices.ru/nestjs-backend/auth", {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json;charset=utf-8'
-                      },
-                    body: JSON.stringify(newForm)
-                    }).then(response => response.json()).then(result => {
-                        console.table(result)
-                    });
+                xhr.open("POST","https://students.netoservices.ru/nestjs-backend/auth");
+                xhr.responseType = "json";
+                xhr.send(newForm);
+                xhr.addEventListener("readystatechange" , () => {
+                    if (xhr.readyState === xhr.DONE) {
+                       if (xhr.response.success) {
+                            authForm.parentElement.classList.remove("signin_active");
+                            welcome.textContent = "Добро пожаловать : "+xhr.response["user_id"];
+                            welcome.classList.add("welcome_active");
+                            localStorage.setItem("user-"+xhr.response["user_id"],xhr.response["user_id"]);
+                       } else {
+                        Incorrect(authForm);
+                       }
+                    }
+                })
     
             });
+        } else {
+            let id = Check();
+            authForm.parentElement.classList.remove("signin_active");
+            welcome.textContent = "Добро пожаловать : "+ id;
+            welcome.classList.add("welcome_active");
         }
-
 }
-Auth();
+Auth2();
